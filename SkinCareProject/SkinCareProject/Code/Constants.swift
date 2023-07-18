e08daf5188d43b1cebc56e95feef5874eecee5bd
felipeelsnersilva15@gmail.com <Felipe Elsner Silva>
2023-07-18 17:30:10 -0300

    feat: create enums and classes

---
+++ SkinCareProject/SkinCareProject/Code/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  SkinCareProject
+//
+//  Created by Felipe  Elsner Silva on 18/07/23.
+//
+
+import Foundation
+import SwiftUI
+
+class Constants: ObservableObject {
+    static let shared: Constants = Constants()
+    
+    //create here variables
+    
+    
+}
---
+++ SkinCareProject/SkinCareProject/Code/Structs/Enums.swift
@@ -0,0 +1,42 @@
+//
+//  Enums.swift
+//  SkinCareProject
+//
+//  Created by Felipe  Elsner Silva on 18/07/23.
+//
+
+import Foundation
+
+enum Phototype: String {
+    case one = "Tipo 1"
+    case two = "Tipo 2"
+    case three = "Tipo 3"
+    case four = "Tipo 4"
+    case five = "Tipo 5"
+    case six = "Tipo 6"
+    
+    func typeNumber() -> Int{
+        switch self{
+        case .one:
+            return 1
+        case .two:
+            return 2
+        case .three:
+            return 3
+        case .four:
+            return 4
+        case .five:
+            return 5
+        case .six:
+            return 6
+        }
+    }
+}
+
+enum SkinType: String {
+    case oily = "Oleosa"
+    case dry = "Seca"
+    case normal = "Normal"
+    case combination = "Mista"
+    case sensitive = "Sensível"
+}
