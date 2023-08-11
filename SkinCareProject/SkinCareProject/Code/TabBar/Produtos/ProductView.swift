a4e2276a704da49695b0b99f964b003e2bc876ab
felipeelsnersilva15@gmail.com <Felipe Elsner Silva>
2023-08-10 21:21:54 -0300

    fix: nickname calls

--- SkinCareProject/SkinCareProject/Code/TabBar/Produtos/ProductView.swift
+++ SkinCareProject/SkinCareProject/Code/TabBar/Produtos/ProductView.swift
@@ -127,6 +127,6 @@ struct SheetProduct: View {
 
 struct ProductView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductView(product: ListProduct(image: CloudKitUtility.makeURLJPG(image: "ProfileDefault"), name: "Niacinamida", explanation: "", brand: "Simple Organic", recomendedTime: [], vegan: true, barcode: 123, priceRange: 100, SPF: 1, texture: "", ingredients: [], categories: [])!)
+        ProductView(product: ListProduct(image: CloudKitUtility.makeURLJPG(image: "ProfileDefault"), name: "Niacinamida", nickname: "Niacinamida", explanation: "", brand: "Simple Organic", recomendedTime: [], vegan: true, barcode: 123, priceRange: 100, SPF: 1, texture: "", ingredients: [], categories: [])!)
     }
 }
